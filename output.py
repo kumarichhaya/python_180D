393c3853db79148fc707fb3ab2ec7af93bbedf59
kumarichhaya <chhaya18180@gmail.com>
2025-11-09 22:10:50 +0530

    output

---
+++ output.py
@@ -0,0 +1,13 @@
+
+# Run >>  python output.py
+name = input('name:')
+print(name)
+
+num = int(input('Enter a number :-'))
+print(num)
+
+# print(name+num)  # TypeError: can only concate str not int to str 
+
+num1 = float(input('Enter a  Float number :-'))
+print(num1)
+
